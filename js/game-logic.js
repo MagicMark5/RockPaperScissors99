309fbce7b2153083e6926fcfbafa85d5223af1c0
Adam Marsala <adammarsala@hotmail.com>
2021-05-18 15:50:17 -0400

    implement setPlayerMoves main functionality and edge cases

--- js/game-logic.js
+++ js/game-logic.js
@@ -49,20 +49,109 @@ const playerTwoMoves = [
   }
 ];
 
-// Game Functions
-const setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
- 
+const playerMoves = {
+  'Player One': playerOneMoves,
+  'Player Two': playerTwoMoves
 }
 
+// Game Functions
+
+const setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
+  
+  const moveTypes = ['rock', 'paper', 'scissors'];
+  const typeArgs = [moveOneType, moveTwoType, moveThreeType];
+  const valueArgs = [moveOneValue, moveTwoValue, moveThreeValue];
+  let valueSum = 0;
+
+  // Check if rock, paper, and scissors were provided as strings
+  for (const type of typeArgs) {
+    if (!moveTypes.includes(type)) {
+      return;
+    }
+  }
+
+  // Check if all 3 values are provided as numbers 1-99
+  // and that their sum does not exceed 99
+  for (const value of valueArgs) {
+    valueSum += value; 
+    if (typeof value !== 'number' || value < 1 || value > 99 || valueSum > 99) {
+      return;
+    }
+  }
+  
+    
+  if (player === 'Player One') {
+    // set move 1
+    playerOneMoveOneType = moveOneType;
+    playerOneMoveOneValue = moveOneValue;
+    // set move 2
+    playerOneMoveTwoType = moveTwoType;
+    playerOneMoveTwoValue = moveTwoValue;
+    // set move 3
+    playerOneMoveThreeType = moveThreeType;
+    playerOneMoveThreeValue = moveThreeValue;
+  } else if (player === 'Player Two') {
+    // set move 1
+    playerTwoMoveOneType = moveOneType;
+    playerTwoMoveOneValue = moveOneValue;
+    // set move 2
+    playerTwoMoveTwoType = moveTwoType;
+    playerTwoMoveTwoValue = moveTwoValue;
+    // set move 3
+    playerTwoMoveThreeType = moveThreeType;
+    playerTwoMoveThreeValue = moveThreeValue;
+  }
+  
+}
+
+// const validateArgs = (args) => {
+//   // returns boolean if provided argument array contains
+//   // valid arguments for the setPlayerMoves function
+
+//   let valueSum = 0; // All three values should not sum to more than 99
+
+//   for (let i = 1; i < args.length; i++) {
+//     if (i % 2 === 0) {
+//       // even indexed arguments should be Values from 1-99
+//       if (typeof args[i] !== 'number') return false; 
+//       // increment the sum
+//       valueSum += args[i];
+//       // check the values
+//       if (args[i] < 1 || argument > 99 || valueSum > 99) {
+//         return false;
+//       }
+//     } else if (i % 2 !== 0) {
+//       // odd indexed arguments should be types of either 'rock', 'paper', 'scissors'
+//       const acceptedTypes = ['rock', 'paper', 'scissors'];
+//       if (!acceptedTypes.includes(argument)) {
+//         return false;
+//       }
+//     }
+//   }
+
+//   return true
+// };
 
 
 
 const clearMoves = () => {
   // Reset all globals to undefined 
-  
-  // for (let i = 0; i < playerOneMoves.length; i++) {
-  //   playerOneMoves[i].type = undefined;
 
-  // }
-  
+  // Types - Player One
+  playerOneMoveOneType = undefined;
+  playerOneMoveTwoType = undefined;
+  playerOneMoveThreeType = undefined;
+  // Types - Player Two
+  playerTwoMoveOneType = undefined;
+  playerTwoMoveTwoType = undefined;
+  playerTwoMoveThreeType = undefined;
+
+  // Values - Player One
+  playerOneMoveOneValue = undefined;
+  playerOneMoveTwoValue = undefined;
+  playerOneMoveThreeValue = undefined;
+  // Values - Player Two
+  playerTwoMoveOneValue = undefined;
+  playerTwoMoveTwoValue = undefined;
+  playerTwoMoveThreeValue = undefined;
 }
