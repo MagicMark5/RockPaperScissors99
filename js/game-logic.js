5cb933b0e1252509021e28c82ee8a0062ebad779
Adam Marsala <adammarsala@hotmail.com>
2021-05-18 13:22:37 -0400

    Define global player one/two variables to undefined

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,1 +1,20 @@
-// All code should be written in this file.
+// Global variables Types - a string (e.g. Rock, Paper, Scissors
+// and Values - a number (e.g. 1-99)
+
+// Types - Player One
+const playerOneMoveOneType = undefined;
+const playerOneMoveTwoType = undefined;
+const playerOneMoveThreeType = undefined;
+// Types - Player Two
+const playerTwoMoveOneType = undefined;
+const playerTwoMoveTwoType = undefined;
+const playerTwoMoveThreeType = undefined;
+
+// Values - Player One
+const playerOneMoveOneValue = undefined;
+const playerOneMoveTwoValue = undefined;
+const playerOneMoveThreeValue = undefined;
+// Values - Player Two
+const playerTwoMoveOneValue = undefined;
+const playerTwoMoveTwoValue = undefined;
+const playerTwoMoveThreeValue = undefined;
